b03ac25338748c4485562c1fc102236030aa0d24
RahulKuchil <81630490+RahulKuchil@users.noreply.github.com>
2022-09-12 12:30:41 +0530

    Write a script to print the files which has read write and execute permission

---
+++ permisson.sh
@@ -0,0 +1,10 @@
+for file in *
+do
+        if [ -f $file ]
+        then
+                if [ -r $file -a -w $file -a -x $file ]
+                then
+                ls -l $file
+                fi
+        fi
+done
