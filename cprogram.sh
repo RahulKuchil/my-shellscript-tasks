d6066f3f97699c891410ea08a41c64492285440a
RahulKuchil <81630490+RahulKuchil@users.noreply.github.com>
2022-09-12 10:19:00 +0530

    Write a C program in shell script

---
+++ cprogram.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "
+#include<stdio.h>                       #c program
+int main()
+{
+        printf( \" Hello World\n \" );    
+        return 0;
+}
+" > script1.c                           #file to append
